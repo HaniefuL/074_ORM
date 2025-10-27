60810fb4139d703548192297a3645842bffa70b5
HaniefuL <haniefulgeofarry@gmail.com>
2025-10-27 09:39:29 +0700

    mengganti eksport menjadi expres di index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const exports = require('express')
+const express = require('express')
 const app = express();
 const PORT = 3000;
-const db = require("./models");
+const db = require('./models');
 app.use(express.json());
 app.use(express.urlencoded({
     extended: false
@@ -20,4 +20,3 @@ db.sequelize.sync()
     .catch((err) => {
         console.log(err);
     })
-    
